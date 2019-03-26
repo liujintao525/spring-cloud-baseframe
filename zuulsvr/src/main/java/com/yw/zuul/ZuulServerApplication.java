dfb92830fc76a3200ff2df6a023a37b2658e9fd8
liujintao <liujintao525@qq.com>
2019-03-26 16:45:07 +0800

    add zuulsvr and fixed zuul config

---
+++ zuulsvr/src/main/java/com/yw/zuul/ZuulServerApplication.java
@@ -0,0 +1,42 @@
+package com.yw.zuul;
+
+
+import com.yw.zuul.utils.UserContextInterceptor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
+import java.util.List;
+
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+@RefreshScope
+public class ZuulServerApplication {
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        RestTemplate template = new RestTemplate();
+        List interceptors = template.getInterceptors();
+        if (interceptors == null) {
+            template.setInterceptors(Collections.singletonList(new UserContextInterceptor()));
+        } else {
+            interceptors.add(new UserContextInterceptor());
+            template.setInterceptors(interceptors);
+        }
+
+        return template;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulServerApplication.class, args);
+    }
+}
+
